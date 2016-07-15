b5ac9743379e8d39bbae68137ca28182baf24b05
Hayri Cicek <hello@hayricicek.se>
2016-07-15 18:34:23 +0200

    added celery tasks to project

--- myshop/celery.py
+++ myshop/celery.py
@@ -2,8 +2,8 @@ import os
 from celery import Celery
 from django.conf import settings
 
-# set the default Django settings module for the 'celery' program
-os.environ.setdefault('DJANGO_SETTINGS_MODUEL', 'myshop.settings')
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 app = Celery('myshop')
 
--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,8 @@ from .forms import OrderCreateForm
 
 from cart.cart import Cart
 
+from .tasks import order_created
+
 
 def order_create(request):
     cart = Cart(request)
@@ -19,6 +21,9 @@ def order_create(request):
                                         quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            
             return render(request,
                         'orders/order/created.html',
                         {'order': order})
