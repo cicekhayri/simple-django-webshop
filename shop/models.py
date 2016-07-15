42b6fe60574281fde3caed38f4ce1d8ed0045c02
Hayri Cicek <hello@hayricicek.se>
2016-07-15 10:55:26 +0200

    added stock field to shop/Product model

--- shop/models.py
+++ shop/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
